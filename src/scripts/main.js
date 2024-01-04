827eaddda5633bcdf51d456db7f306a762d2715c
Juan Camelo <jfcamelo.2004@gmail.com>
2024-01-04 12:40:35 -0500

    Agregando estilos y lista de trending movies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log(API_KEY)
+const BASE_URL = 'https://api.themoviedb.org/3/'
+
+const trendingPrevContainer = document.querySelector('.trendingPreview-movieList')
+
+async function getTrendingMoviesPreview() {
+    const res = await fetch(BASE_URL + 'trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const trendingMoviesRaw = data.results
+
+    let htmlContent = ''; 
+    trendingMoviesRaw.forEach(movie => {
+        htmlContent += `
+        <div class="movie-container">
+            <img
+                src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+                class="movie-img"
+                alt="${movie.title}"
+            />
+        </div>
+        `
+    });
+
+    trendingPrevContainer.innerHTML = htmlContent;
+}getTrendingMoviesPreview()
